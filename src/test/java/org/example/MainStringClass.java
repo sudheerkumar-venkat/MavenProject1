330db7aa43a3febb302b516c1c9346f33f954310
sudheerkumardhushu <sudheerkumardushu@gmail.com>
2025-09-19 11:53:36 +0530

    adding new file

---
+++ src/test/java/org/example/MainStringClass.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class MainStringClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//CharAtClass.enterTextFromKeyboard();
+		//CharAtClass.ChartAtMethod();
+
+	}
+
+}
